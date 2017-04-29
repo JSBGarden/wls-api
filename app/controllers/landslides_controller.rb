f39af1699c2b6a2167188b434c535b4762ce7e0a
Batsal Awaley <batsal@vayu.com.au>
2017-04-29 15:08:32 +0545

    Add resource landslide

--- app/controllers/landslides_controller.rb
+++ app/controllers/landslides_controller.rb
@@ -1,19 +1,54 @@
 class LandslidesController < ApplicationController
-
-  def index
+  def new
+    @landslide = Landslide.new
   end
 
-  def new
+  def update
+    @landslide = Landslide.find(params[:id])
+  end
+
+  def index
+    @landslides = Landslide.all
+  end
+
+  def show
+    @landslide = Landslide.find(params[:id])
   end
 
   def create
+    @landslide = Landslide.new(landslide_params)
+    # @landslide.user_id = current_user.id
+
+    respond_to do |format|
+      if @landslide.save
+
+        format.json {
+          render "show"
+        }
+      else
+        puts @landslide.errors
+        format.json { render :json => { :errors => @landslide.errors },  :status => :unprocessable_entity }
+      end
+    end
   end
-  def edit
-  end
+
+
   def update
+    @landslide = Landslide.find(params[:id])
+
+    respond_to do |format|
+      if @landslide.update_attributes(landslide_params)
+        format.json {
+          render "show"
+        }
+      else
+        format.json { render :json => { :errors => @landslide.errors },  :status => :unprocessable_entity }
+      end
+    end
+
   end
 
-  def delete
+  def destroy
+    respond_with Landslide.destroy(params[:id])
   end
-
 end
